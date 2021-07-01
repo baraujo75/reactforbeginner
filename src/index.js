21466bb4ae60314e4d0dffd8903e01f50b6dab34
Bruno Araujo <bruno.bvaraujo@gmail.com>
2021-07-01 17:59:11 -0300

    Added CSS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import App from './App';
 
-function App() {
-  return React.createElement(
-    'a', 
-  {href: 'https://www.origamid.com'},
-  'Origamid');
-}
-
-ReactDOM.render(App(), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
